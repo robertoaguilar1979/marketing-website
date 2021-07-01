5ac22a7ae2de04482fe8b49de4a555356f72abbe
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-07-01 01:01:33 -0700

    added all files to the marketing website

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const closeButton = document.querySelector(".close-nav");
+const openButton = document.querySelector(".bars");
+const nav = document.querySelector(".nav");
+
+
+// close navigation button onclick
+closeButton.addEventListener("click", () => {
+    nav.classList.remove('navigation-open');
+});
+
+// open navigation button onclick
+openButton.addEventListener("click", () => {
+    nav.classList.add("navigation-open");
+});
