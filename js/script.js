2f37953736390f38cba445597870b169d0631b65
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-07-10 10:57:44 -0700

    fixed the javascript issue

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const closeButton = document.querySelector(".close-nav");
 const openButton = document.querySelector(".bars");
 const nav = document.querySelector(".nav");
 
-
 // close navigation button onclick
 closeButton.addEventListener("click", () => {
     nav.classList.remove('navigation-open');
