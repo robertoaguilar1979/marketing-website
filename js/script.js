6cd44db1e578ed9e8e8a0006a25cc061cb0c1cb7
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-08-22 20:10:59 -0700

    fixed javascript remove navigation on mobile when clicked, and fixed main content on index area

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const closeButton = document.querySelector(".close-nav");
 const openButton = document.querySelector(".bars");
 const nav = document.querySelector(".nav");
+const list = document.querySelectorAll(".list");
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", () => {
+        nav.classList.remove("navigation-open");
+    })
+}
 
 // close navigation button onclick
 closeButton.addEventListener("click", () => {
